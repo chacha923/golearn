b932313c070da65bd863c4794217f5649ff5929b
chacha923 <chacha923@aliyun.com>
2023-05-06 19:38:57 +0800

    fix lis

--- suanfa/dp/lis.go
+++ suanfa/dp/lis.go
@@ -1,13 +1,10 @@
 package dp
 
-import (
-	"fmt"
-)
-
 //求数组的最长递增子序列 (lis)  1.滑动窗口法 2.动态规划
 //另一种解法: 拷贝数组并排序, 转化为求最长公共子序列lcs 的 长度
 //状态转移方程:
-// d(i) = max{d(j)+1, 1} (当且仅当  j < i , arr[j] < arr[i])
+// d(i) = max{d(j)+1, 1}
+// 当 arr[i] > arr[j], 那么 dp[i] = dp[j] + 1, 否则 dp[i] = 1; 其中 j 是小于 i 的最大下标
 
 // 用大白话解释就是，想要求d(i)，就把i前面的各个子序列中，
 // 最后一个数不大于A[i]的序列长度加1，然后取出最大的长度即为d(i)。
@@ -16,30 +13,31 @@ import (
 var res = make([]int, 0)
 
 //求 数组的 lis 长度
-func lisLength(arr []int, length int) {
-	lis := make([]int, 0, length) //lis数组保存 0~index 子数组的 lis长度
+func lisLength(arr []int, length int) int {
+	// dp[i] 表示以第 i 个元素为结尾的最长递增子序列的长度
+	dp := make([]int, length)
 	//init 初始化, 子数组的lis值默认为1
-	for k := range lis {
-		lis[k] = 1
+	for k := range dp {
+		dp[k] = 1
 	}
 
 	for i := 1; i < length; i++ {
 		for j := 0; j < i; j++ {
-			if arr[i] > arr[j] && lis[i] < lis[j]+1 {
-				lis[i] = lis[j] + 1
+			if arr[i] > arr[j] {
+				dp[i] = dp[j] + 1
 			}
 		}
 	}
 
-	//找出lis[]的最大值
-	max := lis[0]
+	//找出dp[]的最大值
+	max := dp[0]
 	for i := 1; i < length; i++ {
-		if max < lis[i] {
-			max = lis[i]
+		if max < dp[i] {
+			max = dp[i]
 		}
 	}
 
-	fmt.Println(max)
+	return max
 }
 
 func lisLengthWindow(arr []int) int {
