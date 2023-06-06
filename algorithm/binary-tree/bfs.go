80e6e0335285688077211264468a776f54449ad0
mamori23 <mamori23@dingtalk.com>
2023-06-06 18:37:10 +0800

    WidthOfBinaryTree

--- algorithm/binary-tree/bfs.go
+++ algorithm/binary-tree/bfs.go
@@ -11,41 +11,35 @@ import "golearn/algorithm/structure"
 // 这都是遍历一个集合的方式，目的一样，只是实现方式不同。
 // 我喜欢深度优先，因为可以使用递归，简单。但广度优先很容易使用并发算法优化。
 // 如果有一种极端情况，这个结构很大，使用递归会耗尽函数的栈空间。
-// 这个时候，深度优先算法就不能用了，就要考虑使用广度优先算法。
-
-// 层次遍历
+// 这个时候，深度优先算法就不能用了，就要考虑使用广度优先算法（层次遍历）。
 
 // 二叉树最大宽度
 // 给定一个二叉树，编写一个函数来获取这个树的最大宽度。树的宽度是所有层中的最大宽度。
 // 每一层的宽度被定义为两个端点（该层最左和最右的非空节点，两端点间的null节点也计入长度）之间的长度。
-// 这题比较特殊，需要事先按层次遍历给二叉树标记下标，
-func widthOfBinaryTree(root *TreeNode) int {
-	// 节点指针+下标构成的元组, golang没有元组用结构体代替
-	type Item struct {
-		idx int
-		*TreeNode
-	}
-
+func WidthOfBinaryTree(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
 	ans := 1
-	queue := []Item{{0, root}} // bfs使用的队列
-	for len(queue) > 0 {
-		l := queue[len(queue)-1].idx - queue[0].idx + 1
+	// queue := []Item{{0, root}} // bfs使用的队列
+	var queue = structure.NewQueue[*TreeNode]()
+	queue.Push(root)
+	for queue.Len() > 0 {
+		// 当前层的宽度
+		l := queue.Len()
 		if l > ans {
 			ans = l
 		}
-		tmp := []Item{} // 临时队列, 使得每一次循环, queue内包含同一层的节点(分层遍历?)
-		for _, q := range queue {
-			if q.Left != nil {
-				tmp = append(tmp, Item{q.idx * 2, q.Left})
+		// 每一次循环, queue内包含同一层的节点(分层遍历?)
+		for i := 0; i < queue.Len(); i++ {
+			var pop = queue.Pop()
+			if pop.Left != nil {
+				queue.Push(pop.Left)
 			}
-			if q.Right != nil {
-				tmp = append(tmp, Item{q.idx*2 + 1, q.Right})
+			if pop.Right != nil {
+				queue.Push(pop.Right)
 			}
 		}
-		queue = tmp
 	}
 	return ans
 }
