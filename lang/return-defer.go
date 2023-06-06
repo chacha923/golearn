c0d78c3cd008d60cfbe3015820f3b8136dcf60b7
mamori23 <mamori23@dingtalk.com>
2023-06-06 18:09:43 +0800

    remove

--- lang/return-defer.go
+++ lang/return-defer.go
@@ -2,7 +2,7 @@ package lang
 
 import "fmt"
 
-func ReturnBeforeDefer() string{
+func ReturnBeforeDefer() string {
 	return "return above defer"
 	defer fmt.Println("defer keyword")
 	return "return below defer"
