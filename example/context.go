ad1de1476edaf8dd4df04de7cda03e83b2700aee
mamori23 <mamori23@dingtalk.com>
2023-07-03 00:41:19 +0800

    example: context signal

---
+++ example/context.go
@@ -0,0 +1,36 @@
+package example
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+	fmt.Println("server: hello handler started")
+	defer fmt.Println("server: hello handler ended")
+
+	select {
+	case <-time.After(10 * time.Second):
+		fmt.Fprintf(w, "hello\n")
+	case <-ctx.Done():
+		err := ctx.Err()
+		fmt.Println("server:", err)
+		internalError := http.StatusInternalServerError
+		http.Error(w, err.Error(), internalError)
+	}
+}
+
+func server() {
+	http.HandleFunc("/hello", hello)
+	http.ListenAndServe(":8090", nil)
+}
+
+/*
+$ curl localhost:8090/hello
+server: hello handler started
+^C
+server: context canceled
+server: hello handler ended
+*/
--- example/signal.go
+++ example/signal.go
@@ -1,6 +1,36 @@
 package example
 
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
 // 处理系统信号
 func ListenSignal() {
-	
+	sigs := make(chan os.Signal, 1)
+
+	// signal.Notify 注册给定的通道，用于接收特定信号。
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	done := make(chan bool, 1)
+
+	go func() {
+		sig := <-sigs
+		fmt.Println()
+		fmt.Println(sig)
+		done <- true
+	}()
+
+	fmt.Println("awaiting signal")
+	<-done
+	fmt.Println("exiting")
+
+	/*
+		awaiting signal
+		^C
+		interrupt
+		exiting
+	*/
 }
